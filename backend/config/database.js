292605b2e18cb713b87120ec333d0f9e14b1764d
Pedro Gomes <pedrogac@hotmail.com>
2017-09-25 21:54:36 -0300

    Alteração para conexão com o banco on-line mLab

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,10 @@
 //Conexao com o banco de dados, não precisa informar usuario e senha
 
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_projeto_aeso')
+//module.exports = mongoose.connect('mongodb://localhost/db_projeto_aeso')
+
+
+module.exports = mongoose.connect('mongodb://pedro_gomes:Aeso_2203@ds147884.mlab.com:47884/db_projeto_aeso');
 
 /* mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
