f0092d343e442ae0765abfda5f7fd190388dd582
pgomes22 <pedrogac@hotmail.com>
2017-10-03 19:25:02 -0300

    frontend

---
+++ frontend/client/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: './app.component.html'
+})
+
+export class AppComponent{
+
+    fotos: Array<Object> = [];
+
+    constructor(http: Http) {
+
+/*         let stream = http.get('http://localhost:3003/api/pedido');
+        stream.subscribe(function(res) {
+            this.fotos = res.json();
+            console.log(this.fotos);
+        }, erro => console.log(erro)); */
+
+        http
+        .get('http://localhost:3003/api/produto')
+        .map(res => res.json())
+        .subscribe(fotos => {
+            this.fotos = fotos;
+            console.log(this.fotos);            
+        }, erro => console.log(erro));
+
+    }
+}
