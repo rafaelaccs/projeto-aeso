831a37c5d4c252428860307b0ac969625e2f6f4d
pgomes22 <pedrogac@hotmail.com>
2017-10-03 20:20:10 -0300

    Atualizando Retrospectiva

--- backend/config/server.js
+++ backend/config/server.js
@@ -9,6 +9,12 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.listen(port, function(){
   console.log(`Backend is running on port ${port}.`)
 })
