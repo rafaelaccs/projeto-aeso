dddf7718904070abf47017d9646fbca667ee5c9d
pgomes22 <pedrogac@hotmail.com>
2017-10-23 21:36:22 -0300

    atualizacao

--- backend/api/barGenius/funcionalidades/fc_produtos_estabelecimento.js
+++ backend/api/barGenius/funcionalidades/fc_produtos_estabelecimento.js
@@ -8,24 +8,27 @@ ProdEstabelecimento.before('get', function(req, res, next){
 
     const estab_id = req.params.id;
     console.log("Estabelecimento: ", estab_id);
-        
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-    
-        const estabecimento = db.collection("produtos")
-                              .find({estabelecimento_id: ObjectId(estab_id)})
-                              .toArray(function(err, result) {
-    
-        if (err) throw err;
-           
-        db.close();
-        return (res.json(result));
-        
-    
-        });
-    });
 
-    //next();
+    if(estab_id.length > 1){
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+        
+            const estabecimento = db.collection("produtos")
+                                .find({estabelecimento_id: ObjectId(estab_id)})
+                                .toArray(function(err, result) {
+        
+            if (err) throw err;
+            
+            db.close();
+            return (res.json(result));
+            
+        
+            });
+        });
+    }else{
+        ProdEstabelecimento.methods(['get', 'post', 'put', 'delete']);
+        next();
+    }    
 });
 
 ProdEstabelecimento.route('count', function(req, res, next){
